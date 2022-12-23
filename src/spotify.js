a4540ddf31a5b3a98d650d038090f72a015b1181
Ashish <ashishsawant2150@gmail.com>
2022-12-23 21:23:20 +0530

    added env

--- src/spotify.js
+++ src/spotify.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
-const client_id = 'b8aca1007f284e1d80bf93f9f0c6d0a1'; // Your client id
-// const client_id = '693285c4d29b4fc99480403f400d2179'; // Your client id ---hosted app 
-const redirect_uri = 'http://localhost:3000'; // Your redirect uri
-// const redirect_uri = 'https://tseries.netlify.app'; // Your redirect uri ---hosted app
+const client_id = process.env.REACT_APP_CLIENT_ID; // client id ---hosted app 
+const redirect_uri = 'https://tseries.netlify.app'; //  redirect uri ---hosted app
 const authEndpoint='https://accounts.spotify.com/authorize?'
 const scopes=['user-library-read','playlist-read-private']
 
